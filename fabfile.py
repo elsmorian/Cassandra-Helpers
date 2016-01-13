d3e552b47db6d86dd49ab87d6aff3332743c5109
Chris Elsmore <chris.elsmore@demandlogic.co.uk>
2016-01-13 14:58:57 +0000

    Added more Cassandra helper methods

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,14 @@
 Fabric commands to automate some Cassandra stuff.
 """
 import json
+import time
+import getpass
 
-from fabric.api import run, execute, task, env, hosts, parallel
+from fabric.api import run, sudo, execute, task, env, hosts, parallel
+from fabric.operations import prompt
 from fabric.state import output
-from fabric.colors import green, white
+from fabric.utils import abort
+from fabric.colors import green, white, yellow
 from fabric.network import ssh
 
 CASSANDRA_CLUSTER_DETAILS = 'cassandra_cluster_details.json'
@@ -28,19 +32,89 @@ def _print_cassandra_version():
     node_print("Version:", version)
 
 @parallel
+def _print_cassandra_load():
+    """Outputs the current data load of the Cassandra node."""
+    result = run('nodetool info | grep Load')
+    load = result.split(':')[1].strip()
+    node_print("Load:", load)
+
+@parallel
+def _print_cassandra_stream_throughput():
+    """Outputs the current streaming throughput limit of the Cassandra node."""
+    result = run('nodetool getstreamthroughput')
+    throughput = result.split(':')[1].strip()
+    node_print("Stream throughput limit:", throughput)
+
+@parallel
+def _print_cassandra_compaction_throughput():
+    """Outputs the current compaction throughput limit of the Cassandra node."""
+    result = run('nodetool getcompactionthroughput')
+    throughput = result.split(':')[1].strip()
+    node_print("Compaction throughput limit:", throughput)
+
+@parallel
+def _print_cassandra_memory():
+    """Outputs the current memory info of the Cassandra node."""
+    result = run("nodetool info | grep 'Heap Memory'")
+    heap_memory_string = result.split('\n')[0]
+    off_heap_memory_string = result.split('\n')[1]
+    heap_memory = heap_memory_string.split(':')[1].strip()
+    off_heap_memory = off_heap_memory_string.split(':')[1].strip()
+    node_print("Heap Memory (MB):", heap_memory)
+    node_print("Off Heap Memory (MB):", off_heap_memory)
+    print "\n"
+
+@parallel
 def _disable_auto_compactions(keyspace):
     """Disables automatic compactions for the given keyspace."""
     result = run('nodetool disableautocompaction -- {}'.format(keyspace))
     status = red(result.failed) if result.failed else green(not result.failed)
-    node_print("Compaction disabled for '{}'".format(keyspace), status)
+    node_print("Compaction disabled for '{}':".format(keyspace), status)
 
 @parallel
 def _enable_auto_compactions(keyspace):
     """Enables automatic compactions for the given keyspace."""
-    result = run('nodetool enableautocompaction --{}'.format(keyspace))
+    result = run('nodetool enableautocompaction -- {}'.format(keyspace))
     status = red(result.failed) if result.failed else green(not result.failed)
-    node_print("Compaction enabled for '{}'".format(keyspace), status)
+    node_print("Compaction enabled for '{}':".format(keyspace), status)
 
+
+@parallel
+def _set_stream_throughput_limit(limit=200):
+    """Enables the stream throughput limit to the given throughput in mbit/s."""
+    run('nodetool setstreamthroughput -- {}'.format(limit))
+    _print_cassandra_stream_throughput()
+
+@parallel
+def _set_compaction_throughput_limit(limit=64):
+    """Enables the compaction throughput limit to the given throughput in MB/s."""
+    run('nodetool setcompactionthroughput -- {}'.format(limit))
+    _print_cassandra_compaction_throughput()
+
+@parallel
+def _flush_memtables():
+    """Flushes Cassandra's memtables to disk."""
+    result = run('nodetool flush')
+    status = red(result.failed) if result.failed else green(not result.failed)
+    node_print("Memtables flushed:", status)
+
+def _drain_cassandra_node():
+    """Drains a Cassandra node."""
+    result = run('nodetool drain')
+    status = red(result.failed) if result.failed else green(not result.failed)
+    node_print("Drained:", status)
+
+def _restart_cassandra_node():
+    """Restarts the Cassandra process on a node."""
+    result = sudo('service cassandra restart')
+    node_print("Restarted:", result)
+
+def _check_cassandra_is_running():
+    """Checks Cassandra is running on a node."""
+    result = run('service cassandra status')
+    if result.failed or '* Cassandra is running' not in result:
+        abort(yellow(result))
+    node_print("Status:", result)
 
 def get_all_cassandra_nodes():
     """Returns a list of all the nodes in the Cassandra cluster."""
@@ -67,6 +141,30 @@ def show_cluster_versions():
     print "\n"
 
 @task
+def show_cluster_load():
+    """Show the data load for each Cassandra node in the cluster."""
+    print "\n"
+    print white("Data Load:")
+    execute(_print_cassandra_load, hosts=get_all_cassandra_nodes())
+    print "\n"
+
+@task
+def show_cluster_streaming_throughput():
+    """Show the streaming throughput limit for each Cassandra node."""
+    print "\n"
+    print white("Streaming throughput Limit:")
+    execute(_print_cassandra_stream_throughput, hosts=get_all_cassandra_nodes())
+    print "\n"
+
+@task
+def show_cluster_memory():
+    """Show the memory info for each Cassandra node in the cluster."""
+    print "\n"
+    print white("Cluster Memory:")
+    execute(_print_cassandra_memory, hosts=get_all_cassandra_nodes())
+    print "\n"
+
+@task
 def disable_auto_compactions(keyspace=None):
     """Disables automatic compaction for the given keyspace in the cluster."""
     print "\n"
@@ -82,6 +180,9 @@ def disable_auto_compactions(keyspace=None):
 def enable_auto_compactions(keyspace=None):
     """Enables automatic compaction for the given keyspace in the cluster."""
     print "\n"
+    if keyspace is None:
+        abort(yellow("Please specify a keyspace!"))
+
     print white("Enabling Automatic Compactions:")
     execute(
         _enable_auto_compactions,
@@ -89,3 +190,79 @@ def enable_auto_compactions(keyspace=None):
         hosts=get_all_cassandra_nodes(),
     )
     print "\n"
+
+@task
+def set_streaming_throughput(limit=200):
+    """Sets the streaming throughput limit across the Cassandra cluster."""
+    print "\n"
+    print white("Setting streaming throughput limit to {}:".format(limit))
+    execute(
+        _enable_stream_throughput_limit,
+        limit=limit,
+        hosts=get_all_cassandra_nodes(),
+    )
+    print "\n"
+
+@task
+def disable_streaming_throughput():
+    """Disable the streaming throughput limit across the Cassandra cluster."""
+    print "\n"
+    print white("Disabling streaming throughput limit:")
+    execute(
+        _enable_stream_throughput_limit,
+        limit=0,
+        hosts=get_all_cassandra_nodes(),
+    )
+    print "\n"
+
+@task
+def set_compaction_throughput(limit=64):
+    """Sets the compaction throughput limit across the Cassandra cluster."""
+    print "\n"
+    print white("Setting compaction throughput limit to {}:".format(limit))
+    execute(
+        _enable_compaction_throughput_limit,
+        limit=limit,
+        hosts=get_all_cassandra_nodes(),
+    )
+    print "\n"
+
+@task
+def disable_compaction_throughput():
+    """Disable the compaction throughput limit across the Cassandra cluster."""
+    print "\n"
+    print white("Disabling compaction throughput limit:")
+    execute(
+        _enable_compaction_throughput_limit,
+        limit=0,
+        hosts=get_all_cassandra_nodes(),
+    )
+    print "\n"
+
+@task
+def flush_memtables():
+    """Flushes Cassandra's Memtables to disk on all nodes in the cluster."""
+    print "\n"
+    print white("Flushing Memtables:")
+    execute(_flush_memtables, hosts=get_all_cassandra_nodes())
+    print "\n"
+
+@task
+def run_cluster_rolling_restart():
+    """Restarts each Cassandra node in the cluster one by one."""
+    print "\n"
+    print white("Running rolling cluster restart:")
+    print "\n"
+    for data_center, data_center_nodes in CLUSTER_INFO.iteritems():
+        for node in data_center_nodes:
+            print white("* {}:\n".format(node))
+            env.password = getpass.getpass(prompt="Enter sudo password: ")
+            execute(_drain_cassandra_node, hosts=[node])
+            time.sleep(10)
+            execute(_restart_cassandra_node, hosts=[node])
+            time.sleep(10)
+            execute(_check_cassandra_is_running, hosts=[node])
+            print white("Waiting 60 seconds for gossip and hinted handoffs..")
+            time.sleep(60)
+            print "\n"
+    print white("Rolling restart complete!")
